9947d98edfd7bc9d7856987ca04d50b3ee2cc77d
desarrollo-opting <contabilidad@opting.co>
2021-04-23 10:06:04 -0500

    first commit

---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+import { Redirect, Route } from 'react-router-dom';
+
+export const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => {
+  localStorage.setItem('lastPath', rest.location.pathname);
+  
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to='/login' />
+      }
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  isAuthenticated: Proptypes.bool.isRequired,
+  component: Proptypes.func.isRequired,
+};
